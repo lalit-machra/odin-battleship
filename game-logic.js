28cb61122ea3521d1b4326bb7a0be06a64072d7a
lalit-machra <lalitmachra0403@gmail.com>
2025-03-30 19:05:42 +0530

    Implement GameBoard class

--- game-logic.js
+++ game-logic.js
@@ -19,4 +19,113 @@ class Ship {
   }
 }
 
-export { Ship }
+function makeGameboard() {
+  // 10x10 game board
+  let gameBoard = [];
+  for (let i = 0; i < 10; i++) {
+    gameBoard.push([]);
+    for (let j = 0; j < 10; j++) {
+      gameBoard[i].push([]);
+    }
+  }
+  return gameBoard;
+}
+
+class GameBoard {
+  constructor(playerName) {
+    this.owner = playerName;
+    this.board = makeGameboard();
+    this.shipLocations = [];
+    this.hitShots = [];
+    this.missedShots = [];
+  }
+
+  placeShip(locArr) {
+    // check if all loc values are valid or not
+    let currX, currY, prevX, prevY, diffX, diffY, prevDiffX, prevDiffY;
+    for (let i = 0; i < locArr.length; i++) {
+      currX = locArr[i][0];
+      currY = locArr[i][1];
+      // checking for all values to be in range 0 to 9
+      if ((currX < 0 || currX > 9)
+       || (currY < 0 || currY > 9)) {
+        return false;
+      } else {
+        // checking if place is empty or not
+        if (this.board[currX][currY].length !== 0) {
+          return false;
+        } else {
+          // checking for consecutive places
+          if (i === 0) {
+            // loop runs for first time, set prevX and prevY
+            prevX = currX;
+            prevY = currY;
+          } else {
+            diffX = currX - prevX;
+            diffY = currY - prevY;
+            prevX = currX;
+            prevY = currY;
+            // one of diffX and diffY should be 0 and other should be 1
+            if ((Math.abs(diffX) === 0 && Math.abs(diffY) === 1)
+             || (Math.abs(diffX) === 1 && Math.abs(diffY) === 0)) {
+              if (i === 1) {
+                // loop runs for second time, set prevDiffX and prevDiffY
+                prevDiffX = diffX;
+                prevDiffY = diffY;
+              } else {
+                if ((diffX !== prevDiffX) || (diffY !== prevDiffY)) {
+                  return false;
+                }
+              }
+            } else {
+              return false;
+            }
+          }
+        }
+      }
+    }
+
+    // if the program reaches here, means all checks are passed, so place a
+    // ship at the specified location
+    let newShip = new Ship(locArr.length);
+    let newObj = {'ship': newShip};
+    let shipLocStr;
+    for (let i = 0; i < locArr.length; i++) {
+      this.board[locArr[i][0]][locArr[i][1]].push(newObj);
+      shipLocStr = locArr[i][0].toString() + locArr[i][1].toString();
+      this.shipLocations.push(shipLocStr);
+    }
+    return true;
+  }
+
+  receiveAttack(loc) {
+    let locStr = loc[0].toString() + loc[1].toString();
+    if (this.hitShots.includes(locStr) || this.missedShots.includes(locStr)) {
+      return null;
+    } else {
+      // ship is hit
+      if (this.board[loc[0]][loc[1]].length !== 0) {
+        this.hitShots.push(locStr);
+        let hitShip = this.board[loc[0]][loc[1]][0]["ship"];
+        hitShip.hit();
+        return true;
+      } else {
+        this.missedShots.push(locStr);
+        return false;
+      }
+    }
+  }
+
+  allShipsSunk() {
+    let currShip;
+    for (let i = 0; i < this.shipLocations.length; i++) {
+      currShip = this.shipLocations[i];
+      if (!this.hitShots.includes(currShip)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+export { Ship, GameBoard }
--- game-logic.test.js
+++ game-logic.test.js
@@ -1,4 +1,4 @@
-import { Ship } from './game-logic.js';
+import { Ship, GameBoard } from './game-logic.js';
 
 // Tests for Ship class
 describe('Ship testing', () => {
@@ -35,4 +35,51 @@ describe('Ship testing', () => {
     ship1.hit();
     expect(ship1.sunk).toBe(true);
   });
-}); 
+});
+
+// Tests for gameBoard
+describe('Gameboard testing', () => {
+  let player = 'scot44';
+  let gameBoard = new GameBoard(player);
+  test('place the ship', () => {
+    expect(gameBoard.placeShip([[0, 2], [0, 3]])).toBe(true);
+    expect(gameBoard.placeShip([[0, 1], [0, -3]])).toBe(false);
+    expect(gameBoard.placeShip([[0, 2], [0, 3]])).toBe(false);
+    expect(gameBoard.placeShip([[2, 3], [2, 4], [0, 2]])).toBe(false);   // one cell already occupied
+    expect(gameBoard.placeShip([[2, 3], [2, 5], [2, 6]])).toBe(false);   // not consecutive
+    expect(gameBoard.placeShip([[2, 3], [3, 4], [4, 5]])).toBe(false);   // not consecutive
+    expect(gameBoard.placeShip([[2, 3], [2, 4], [3, 4]])).toBe(false);   // not consecutive
+    expect(gameBoard.placeShip([[9, 2], [9, 3], [9, 4]])).toBe(true);
+    expect(gameBoard.placeShip([[1, 5], [2, 5], [3, 5], [4, 5]])).toBe(true);
+    expect(gameBoard.placeShip([[7, 6], [7, 5], [7, 4], [7, 3]])).toBe(true);
+    expect(gameBoard.placeShip([[3, 8], [2, 8], [1, 8]])).toBe(true);
+  });
+
+  test('receive attack', () => {
+    expect(gameBoard.receiveAttack([4, 5])).toBe(true);
+    expect(gameBoard.receiveAttack([0, 0])).toBe(false);
+    expect(gameBoard.receiveAttack([4, 5])).toBeNull();
+    expect(gameBoard.receiveAttack([0, 0])).toBeNull();
+    expect(gameBoard.receiveAttack([3, 8])).toBe(true);
+    expect(gameBoard.receiveAttack([2, 8])).toBe(true);
+    expect(gameBoard.receiveAttack([2, 8])).toBeNull();
+    expect(gameBoard.receiveAttack([1, 8])).toBe(true);
+  });
+
+  test('all ships sunken', () => {
+    expect(gameBoard.allShipsSunk()).toBe(false);
+    gameBoard.receiveAttack([0, 2]);
+    gameBoard.receiveAttack([7, 3]);
+    gameBoard.receiveAttack([0, 3]);
+    gameBoard.receiveAttack([7, 4]);
+    gameBoard.receiveAttack([9, 3]);
+    gameBoard.receiveAttack([9, 2]);
+    gameBoard.receiveAttack([3, 5]);
+    gameBoard.receiveAttack([7, 5]);
+    gameBoard.receiveAttack([1, 5]);
+    gameBoard.receiveAttack([9, 4]);
+    gameBoard.receiveAttack([2, 5]);
+    gameBoard.receiveAttack([7, 6]);
+    expect(gameBoard.allShipsSunk()).toBe(true);
+  });
+});
