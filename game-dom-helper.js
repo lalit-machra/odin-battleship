403ffdc1857f23c8cdd47e5d4194db69618c55ec
lalit-machra <lalitmachra0403@gmail.com>
2025-03-31 18:35:37 +0530

    Put utility functions in different file to make game-dom.js clean

---
+++ game-dom-helper.js
@@ -0,0 +1,44 @@
+import { Player } from "./game-logic.js";
+
+function disableStartingMenu() {
+  const radioPvc = document.querySelector("#pvc");
+  const radioPvp = document.querySelector("#pvp");
+  const player = document.querySelector("#player");
+  const gameStartBtn = document.querySelector(".gameStartBtn");
+  radioPvc.disabled = true;
+  radioPvp.disabled = true;
+  player.disabled = true;
+  gameStartBtn.disabled = true;
+  return;
+}
+
+function initializeGameBoard(playerArr) {
+  let player, playerDiv, playerNameSpan;
+  for (let i = 0; i < playerArr.length; i++) {
+    player = new Player(playerArr[i]);
+    playerDiv = document.querySelector(`.gameboards .player${i + 1}Board`);
+    playerNameSpan = document.querySelector(`.gameboards .player${i + 1}Name`);
+    displayGameboard(player, playerDiv, playerNameSpan);
+  }
+  return;
+}
+
+function displayGameboard(player, playerDiv, playerNameSpan) {
+  let gameBoard = player.gameBoard.board;
+  console.log(gameBoard);
+  let rowDiv, columnDiv;
+  for (let i = 0; i < gameBoard.length; i++) {
+    rowDiv = document.createElement("div");
+    rowDiv.classList.add("rowDiv");
+    for (let j = 0; j < gameBoard[i].length; j++) {
+      columnDiv = document.createElement("div");
+      columnDiv.classList.add("columnDiv");
+      rowDiv.appendChild(columnDiv);
+    }
+    playerDiv.appendChild(rowDiv);
+  }
+  playerNameSpan.innerText = player.name;
+  return;
+}
+
+export { disableStartingMenu, initializeGameBoard };
